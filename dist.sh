48372339588266eaa75fc703d1b07519ec3754ec
3t0n <god.triton@gmail.com>
2018-10-04 01:13:07 +0300

    Updated list script

--- dist.sh
+++ dist.sh
@@ -2,3 +2,4 @@
 
 # Here should be your path to pyinstaller
 /Library/Frameworks/Python.framework/Versions/3.6/bin/pyinstaller -F --add-binary="ffmpeg:."  mkvinjector.py
+#"C:\Program Files\Python\Scripts\pyinstaller"  -F --add-binary="ffmpeg.exe;."  mkvinjector.py
