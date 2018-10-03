fe9932113e81cd6a02b0e0dcf32f73bf9eea00d7
3t0n <god.triton@gmail.com>
2018-10-04 02:42:13 +0300

    Moved to subprocess. Fixed PyInstaller for windows.

--- mkvinjector.py
+++ mkvinjector.py
@@ -10,9 +10,10 @@ ffmpeg -i input.mkv -i audio.dts -map 0 -map 1 -c copy output.mkv
 """
 
 import sys
+import subprocess
 from distutils.spawn import find_executable
 import os
-from os import listdir, system
+from os import listdir
 from os.path import isfile, join, dirname, abspath, basename, splitext
 
 
@@ -44,29 +45,36 @@ def find_ffmpeg():
     #print(ffmpeg_path)
     return ffmpeg_path
 
-base_cmd = '{0} -loglevel panic -i \"{1}\" {2} -map 0 {3} -c copy \"new-{1}\"'
-
-
 all_files = [f for f in listdir(current_path) if isfile(join(current_path, f))]
 #print (all_files)
 
 mkv_files = [f for f in all_files if f.endswith(".mkv")]
+mkv_files.sort()
 #print (mkv_files)
 
 for mkv in mkv_files:
+    # cmd = ['-i', mkv]
+    cmd = ['-loglevel', 'panic', '-i', mkv]
+
+    print('\n--> Processing \"{}\":'.format(mkv))
+
     mkv_name = splitext(basename(mkv))[0]
     res_files = [f for f in all_files if f != mkv and f.startswith(mkv_name)]
     #print (res_files)
 
     if len(res_files) > 0:
-        input_cmd = ' '.join('-i \"{:s}\"'.format(res) for res in res_files)
-        map_cmd = ' '.join('-map {:d}'.format(i + 1) for i in range(len(res_files)))
-        cmd = base_cmd.format(find_ffmpeg(), mkv, input_cmd, map_cmd)
+        for res in res_files:
+            cmd.extend(['-i', res])
 
-        print('--> Processing \"{}\":\n'.format(mkv))
+        for i in range(len(res_files) + 1):
+            cmd.extend(['-map', str(i)])
+
+        cmd.extend(['-c', 'copy', 'new-' + mkv])
+        cmd.insert(0, find_ffmpeg())
         print(cmd)
 
-        err = system(cmd)
+        err = subprocess.call(cmd, shell=False)
+
         # err 256 == skip rewrite
         if err not in [0, 256]:
             print("Oooops! Something going wrong! Exit code: {0}".format(err))
